d876d2090ff6a7492614d2090dedda8f47219aee
Florea Maria <floreamaria262@gmail.com>
2021-12-04 18:24:08 +0200

    mqtt start

---
+++ src/main/java/MQTTTRY.java
@@ -0,0 +1,19 @@
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.util.UUID;
+
+public class MQTTTRY {
+    public static void main(String[] args) throws MqttException {
+        String publisherId = UUID.randomUUID().toString();
+        IMqttClient publisher = new MqttClient("tcp://iot.eclipse.org:1883",publisherId);
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+        publisher.connect(options);
+    }
+
+}
