843ba1fe40a02da8eb4aa72a733e9c98db67d44c
maria-tudor <62657362+maria-tudor@users.noreply.github.com>
2022-01-26 17:13:22 +0200

    Add files via upload
    --task5 Request la caracteristicile utilizatorului (înal?ime, culoarea ochilor, greutate etc.)

---
+++ UserService.java
@@ -0,0 +1,21 @@
+package com.example.smartwardrobe.service;
+
+import com.example.smartwardrobe.model.Item;
+import com.example.smartwardrobe.model.Outfit;
+import com.example.smartwardrobe.model.User;
+import org.json.simple.JSONArray;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService{
+
+    User saveUser(User user);
+    User findUserById(Long id);
+    Optional<User> findUserByUsername(String username);
+    void writeUserToFile(User user);
+    JSONArray getUsersFromFile();
+    User saveUserFromFile(User user);
+    JSONArray createJsonArrayOfItems(List<Item> items);
+
+}
