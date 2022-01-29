a8ad6b24390e507ce5acb7516dd88b53ed7ab96a
alexandraburu23 <alexandraburuiana23@yahoo.ro>
2022-01-29 03:00:22 +0200

    user features

--- src/main/java/com/example/smartwardrobe/selfie_webcam/Webcam.java
+++ src/main/java/com/example/smartwardrobe/selfie_webcam/Webcam.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 
 public class Webcam {
     public static void main(String[] args) throws IOException {
-        com.github.sarxos.webcam.Webcam webcam = com.github.sarxos.webcam.Webcam.getDefault();
-        webcam.open();
-        ImageIO.write(webcam.getImage(), "JPG", new File("firstcapture.jpg"));
-        webcam.close();
+//        com.github.sarxos.webcam.Webcam webcam = com.github.sarxos.webcam.Webcam.getDefault();
+//        webcam.open();
+//        ImageIO.write(webcam.getImage(), "JPG", new File("firstcapture.jpg"));
+//        webcam.close();
     }
 }
